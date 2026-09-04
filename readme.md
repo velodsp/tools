0cbe24a3cbad3365767de28fdec246af9e341008
underswing <underswing187@gmail.com>
2026-09-04 18:36:10 +0200

    scaffold test-client

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # velodsp-tools
 
 Various tools for interacting with the velodsp firmware.
+
+## test-client
+
+Simple React + Vite web-client for testing the firmware.
---
+++ test-client/src/App.tsx
@@ -0,0 +1,11 @@
+import s from "./App.module.css";
+
+function App() {
+  return (
+    <div className={s.container}>
+      <h1>testing-client</h1>
+    </div>
+  );
+}
+
+export default App;
